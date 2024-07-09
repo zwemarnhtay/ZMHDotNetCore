3803666c8ba4b955e8fb6b48f0fa93e5c1da39a6
hewhounknown <zwemarnhtay@gmail.com>
2024-07-09 19:59:21 +0630

    [added] Canvas Js - CandlestickChart

---
+++ ZMHDotNetCore.MvcChartApp/Controllers/CanvasJsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ZMHDotNetCore.MvcChartApp.Controllers
+{
+    public class CanvasJsController : Controller
+    {
+        public IActionResult CandlestickChart()
+        {
+            return View();
+        }
+    }
+}

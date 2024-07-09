ed404c11aba899c06b5ad5210bc02507fa264f17
hewhounknown <zwemarnhtay@gmail.com>
2024-07-09 20:34:11 +0630

    [completed] serilog

---
+++ ZMHDotNetCore.ConsoleAppLogging/Program.cs
@@ -0,0 +1,25 @@
+﻿using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+			.MinimumLevel.Debug()
+			.WriteTo.Console()
+			.WriteTo.File("logs/ZMHDotNetCore.ConsoleAppLogging.txt", rollingInterval: RollingInterval.Hour)
+			.CreateLogger();
+
+Log.Fatal("Hi Hi");
+Log.Error("err..");
+
+int a = 10, b = 0;
+try
+{
+	Log.Debug("Dividing {A} by {B}", a, b);
+	Console.WriteLine(a / b);
+}
+catch (Exception ex)
+{
+	Log.Error(ex, "Something went wrong");
+}
+finally
+{
+	await Log.CloseAndFlushAsync();
+}
--- ZMHDotNetCore.MvcChartApp/Controllers/HighChatController.cs
+++ ZMHDotNetCore.MvcChartApp/Controllers/HighChatController.cs
@@ -4,8 +4,16 @@ namespace ZMHDotNetCore.MvcChartApp.Controllers
 {
     public class HighChatController : Controller
     {
-        public IActionResult RadialBarChart()
+        private readonly ILogger<HighChatController> _logger;
+
+		public HighChatController(ILogger<HighChatController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult RadialBarChart()
         {
+            _logger.LogInformation("high chart - radial bar chart....");
             return View();
         }
     }

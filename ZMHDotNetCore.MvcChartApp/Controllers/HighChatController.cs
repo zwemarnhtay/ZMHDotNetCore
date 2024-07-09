37af528909c4bda4ede7a9790e63f6638d15dbd5
hewhounknown <zwemarnhtay@gmail.com>
2024-07-09 19:45:43 +0630

    [added] HighChat - RadialBarChart

---
+++ ZMHDotNetCore.MvcChartApp/Controllers/HighChatController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ZMHDotNetCore.MvcChartApp.Controllers
+{
+    public class HighChatController : Controller
+    {
+        public IActionResult RadialBarChart()
+        {
+            return View();
+        }
+    }
+}

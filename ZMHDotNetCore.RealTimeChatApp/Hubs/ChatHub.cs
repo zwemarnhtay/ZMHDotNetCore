58220791942b8c51f224e8e713ba4192b08bf35a
hewhounknown <zwemarnhtay@gmail.com>
2024-07-13 03:48:17 +0630

    [completed] SignalR - realtime Chat

---
+++ ZMHDotNetCore.RealTimeChatApp/Hubs/ChatHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ZMHDotNetCore.RealTimeChatApp.Hubs;
+
+public class ChatHub : Hub
+{
+	public async Task ServerReceiveMessage(string user, string message)
+	{
+		await Clients.All.SendAsync("ClientReceiveMessage", user, message);
+	}
+}

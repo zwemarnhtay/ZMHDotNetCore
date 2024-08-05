c8b82a16d25494f0a16e6f2ced7e66d7534545dd
hewhounknown <zwemarnhtay@gmail.com>
2024-08-06 03:12:58 +0630

    [added] Realtime Chart (SignalR + Scaffold-DbContext)

---
+++ ZMHDotNetCore.RealtimeChartApp/Controllers/PieChartController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ZMHDotNetCore.RealtimeChartApp.Models;
+using ZMHDotNetCore.RealtimeChartApp.Hubs;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ZMHDotNetCore.RealtimeChartApp.Controllers
+{
+    public class PieChartController : Controller
+    {
+        private readonly AppDbContext _db;
+        private readonly IHubContext<ChartHub> _hubContext;
+
+        public PieChartController(AppDbContext db, IHubContext<ChartHub> hubContext)
+        {
+            _db = db;
+            _hubContext = hubContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Save(TblPieChart reqModel)
+        {
+            await _db.TblPieCharts.AddAsync(reqModel);
+            await _db.SaveChangesAsync();
+
+            var lst = await _db.TblPieCharts.AsNoTracking().ToListAsync();
+            var data = lst.Select(x => new PieChartDataModel
+            {
+                name = x.PieChartName,
+                y = x.PieChartValue
+            }).ToList();
+
+            await _hubContext.Clients.All.SendAsync("ReceivePieChart", data);
+
+            return RedirectToAction("Create");
+        }
+    }
+}
+
+public class PieChartDataModel
+{
+    public string name { get; set; }
+    public decimal y { get; set; }
+}
---
+++ ZMHDotNetCore.RealtimeChartApp/Hubs/ChartHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ZMHDotNetCore.RealtimeChartApp.Hubs;
+
+public class ChartHub : Hub
+{
+    public async Task SendMessage(string user, string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+}

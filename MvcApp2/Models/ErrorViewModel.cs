ded8a786e911d7a30dfe7f3f0ca44f438498cae4
hewhounknown <zwemarnhtay@gmail.com>
2024-08-13 19:04:55 +0630

    [created] Blazor web assembly project

--- MvcApp2/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MvcApp2.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
